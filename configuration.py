27010888c88537847e24f27e08c754ba0a0087e5
ArkadiyAk <152608947+ArkadiyAk@users.noreply.github.com>
2023-12-12 19:43:32 +0300

    Create configuration.py

---
+++ configuration.py
@@ -0,0 +1,14 @@
+# путь к серверу (подставляем актуальную ссылку)
+URL_SERVICE = "https://c8aa9bdc-47fc-4a47-aee8-e6730a516142.serverhub.praktikum-services.ru"
+
+# путь к документации api
+DOC_PATH = "/docs/"
+
+# создание пользователя
+CREATE_USER_PATH = "/api/v1/users/"
+
+# проверка пользователя в базе данных
+#users_table_path = "/api/db/resources/user_model.csv"
+
+# создание набора
+CREATE_USER_KIT_PATH = "/api/v1/kits/"
