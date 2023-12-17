d6a811f6abfca5c42d5ef8c178355f3b6593da5a
ArkadiyAk <152608947+ArkadiyAk@users.noreply.github.com>
2023-12-17 15:41:01 +0300

    Update sender_stand_request.py

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -5,9 +5,9 @@ import data          # данные тела из даты
 
 # создание нового пользователя
 def post_new_user(body):
-    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  
-                         json=body,  
-                         headers=data.headers) 
+    return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_PATH,  # полный url
+                         json=body,  # тело
+                         headers=data.headers)  # заголовки
 
 response = post_new_user(data.user_body);
 print(response.status_code)
@@ -16,8 +16,10 @@ print(response.json())
 def post_new_client_kit(kit_body):
     # копирование заголовка из файла data, чтобы не потерять данные в исходном словаре
     headers_dict = data.headers.copy()
-    # передаем параметр authToken
-    headers_dict["Authorization"] = "Bearer " + "authToken";
+    
+    # (исправил) передаем параметр authToken (исправил)
+    headers_dict["Authorization"] = "Bearer " + post_new_user(data.user_body).json()["authToken"];
+    
     # возвращаем ответ созданного набора под авторизованным пользователем
     return requests.post(configuration.URL_SERVICE + configuration.CREATE_USER_KIT_PATH,
                          json=kit_body,
